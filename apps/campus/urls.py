40f5231e29090ac2c7b24709d3a94e10a191048e
Douglas Beck <dbeck@douglasbeck.com>
2011-01-06 16:55:18 -0500

    better url regex for capturing format

--- apps/campus/urls.py
+++ apps/campus/urls.py
@@ -3,6 +3,6 @@ import settings
 
 
 urlpatterns = patterns('campus.views',
-	url(r'^\.?(?P<format>json|kml)?$', 'home', name="home"),
-	url(r'^buildings/\.?(?P<format>json|kml)?$', 'buildings', name="buildings"),
+	url(r'^(\.(?P<format>json|kml))?$', 'home', name="home"),
+	url(r'^buildings/(\.(?P<format>json|kml))?$', 'buildings', name="buildings"),
 )
