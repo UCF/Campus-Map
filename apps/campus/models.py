2a799c4d608c7af9c382bf447644d60ee63237cd
Douglas Beck <dbeck@douglasbeck.com>
2011-06-27 16:24:27 -0400

    adding comments

--- apps/campus/models.py
+++ apps/campus/models.py
@@ -84,6 +84,7 @@ class CommonLocation(models.Model):
 		abstract = True
 
 class Location(CommonLocation):
+	''' I don't like this name.  I want to change it to something else '''
 	slug              = models.SlugField(max_length=255, primary_key=True, help_text='<strong class="caution">Caution</strong>: changing may break external resources (used for links and images)')
 	
 	class Meta:
