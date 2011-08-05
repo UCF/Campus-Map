5d7cd346abefce1a91080c22824743103c64d4c5
Douglas Beck <dbeck@douglasbeck.com>
2011-08-05 16:45:59 -0400

    coordinate import done

--- data/export-import/Import ArcGIS Updates/import.py
+++ data/export-import/Import ArcGIS Updates/import.py
@@ -1,7 +1,7 @@
 # init django project
 import sys, os, json
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-sys.path.append(os.path.abspath('../../'))
+sys.path.append(os.path.abspath('../../../'))
 from apps.campus.models import Building
 from django.core.exceptions import ValidationError
 
@@ -9,59 +9,169 @@ from django.core.exceptions import ValidationError
 import settings
 db = settings.DATABASES['default']['NAME']
 if 'sqlite3' in db:
-	settings.DATABASES['default']['NAME'] = os.path.abspath('../../') + '/' + db
+	settings.DATABASES['default']['NAME'] = os.path.abspath('../../../') + '/' + db
 
-# reset building database from old map
-if "destroy" in sys.argv:
-	
-	print "{0}\n  Destroying all buildings and importing from old  \n{0}".format("-"*78)
-	
-	buildings = Building.objects.all()
-	for b in buildings:
-		b.delete()
-		
-	# import building data from old webcom map, this includes pictures
-	# and point location in both gmap and illustrated map
-	f = open('old_map_dump.json', 'r')
-	txt = f.read()
-	f.close()
-	buildings = json.loads(txt)
-
-	for b in buildings:
-		new = {}
-		new['number']            = b['pk'].lower()
-		new['name']              = b['fields']['name']
-		new['abbreviation']      = b['fields']['abbreviation']
-		new['image']             = b['fields']['image']
-		new['description']       = b['fields']['description']
-		
-		if(		b['fields']['coord_x'] != None 
-			and b['fields']['coord_x'] != ""
-			and b['fields']['coord_y'] != None
-			and b['fields']['coord_y'] != ""
-		): new['googlemap_point']   = "[{0}, {1}]".format(b['fields']['coord_x'], b['fields']['coord_y'])
-		else: new['googlemap_point'] = ""
-		
-		if(		b['fields']['ill_coord_x'] != None 
-			and b['fields']['ill_coord_x'] != "" 
-			and b['fields']['ill_coord_y'] != None
-			and b['fields']['ill_coord_y'] != ""
-		): new['illustrated_point'] = "[{0}, {1}]".format(b['fields']['ill_coord_x'], b['fields']['ill_coord_y'])
-		else: new['illustrated_point'] = None
-		new = Building(**new)
-		new.clean()
-		new.save()
-
-	print
-	print
-
+filename = "buildings_shp_export.json"
+print "Reading contents of", filename, "..."
+print
 
 # update buildings from the authoritative source
-f = open('buildings_export.json')
+f = open(filename)
 txt = f.read()
 f.close()
-buildings = json.loads(txt)
 
+arcgis = json.loads(txt)['features']
+arcgis = sorted(arcgis, key=lambda i:i['properties']['Num'])
+cmap   = list(Building.objects.all())
+
+changed = []
+changes_rejected = []
+deleted_new = []
+deleted_old = []
+errors      = []
+
+def prompt():
+	while(True):
+		i = raw_input("Accept? [y/n] ")
+		if(i == 'y'): 
+			print
+			return True
+		elif(i == 'n'): 
+			print
+			return False
+		else: 
+			print "what?"
+
+def map_url(coords):
+	import urllib
+	if coords == None or coords == "null":
+		return None
+	
+	def flat(l):
+		# recursive function to flatten array and create a a list of coordinates separated by a space
+		str = ""
+		for i in l:
+			if type(i[0]) == type([]):
+				return flat(i)
+			else:
+				str += ("%.6f,%.6f\n")  % (i[0], i[1])
+		return str
+	
+	arr  = json.loads(coords)
+	data = flat(arr)
+	url  = "http://www.gpsvisualizer.com/map_input?form=google&google_wpt_filter_sort=0&form:data=%s%s" % ( 
+	           urllib.quote("longitude,latitude\n"), urllib.quote(data) )
+	return url
+
+
+for ab in arcgis[:]:
+	
+	# look for it campus map
+	for cb in cmap:
+		if ab['properties']['Num'] == cb.number:
+			'''
+			# update name
+			if not ab['properties']['Name'] == cb.name:
+				print "%s [id: %s, abbr: %s]\n%s" % (cb.name, cb.number, cb.abbreviation, '-'*50)
+				print "Name change (from/to):"
+				print ' ', cb.name
+				print ' ', ab['properties']['Name']
+				if(prompt()):
+					b = Building.objects.get( pk=ab['properties']['Num'] )
+					b.name = ab['properties']['Name']
+					b.save()
+				else:
+					changes_rejected.append("Name Change")
+					changes_rejected.append(cb)
+					changes_rejected.append(ab)
+					
+			#update abbreviation
+			if not ab['properties']['Abrev'] == cb.abbreviation:
+				print "%s [id: %s, abbr: %s]\n%s" % (cb.name, cb.number, cb.abbreviation, '-'*50)
+				print "Abreviation change from/to:"
+				print ' ', ("(none)", cb.abbreviation)[bool(cb.abbreviation)]
+				print ' ', ab['properties']['Abrev']
+				if(prompt()):
+					b = Building.objects.get( pk=ab['properties']['Num'] )
+					b.abbreviation = ab['properties']['Abrev']
+					b.save()
+				else:
+					changes_rejected.append("Abbreviation Change")
+					changes_rejected.append(cb)
+					changes_rejected.append(ab)
+			'''
+			
+			#update coords
+			ab_coords = ab['geometry']['coordinates'] if bool(ab['geometry']) else None
+			ab_coords = unicode(json.dumps(ab_coords, ensure_ascii=False ))
+			if not ab_coords == cb.poly_coords:
+				print "%s [id: %s, abbr: %s]\n%s" % (cb.name, cb.number, cb.abbreviation, '-'*50)
+				print "Coordinates changed.  Use the two URLs below to view difference:"
+				print "  - Old Coords: ", map_url(cb.poly_coords)
+				print "  - New Coords: ", map_url(ab_coords)
+				print
+				if(prompt()):
+					# update coords
+					b = Building.objects.get( pk=ab['properties']['Num'] )
+					b.poly_coords  = ab['geometry']['coordinates']
+					try:
+						b.clean()
+						b.save()
+					except ValidationError as e:
+						errors.append(
+							"Unable to save building.\nError: %s\nBuilding: %s\nGeometry: %s\n\n" % (e.messages[0], ab['properties'], ab['geometry']['coordinates'])
+						)
+				else:
+					changes_rejected.append("Coordinates")
+					changes_rejected.append(cb)
+					changes_rejected.append(ab)
+			
+				
+
+
+exit()
+
+'''		
+		
+		if ab['properties']['Num'] == cb['Number']:
+			
+			p
+			
+			
+			# check to see if they're the same
+			for k,v in nb['properties'].items():
+				if not ob['properties'][k] == nb['properties'][k]:
+					changed.append(ob['properties'])
+					changed.append(nb['properties'])
+					break
+			
+			# item exists in old data, remove from both
+			new['features'].remove(nb)
+			old['features'].remove(ob)
+			break
+
+print "{0}\n  Updated Buildings \n{0}".format("-"*78)
+for i in range(0, len(changed), 2):
+	print changed[i]
+	print changed[i+1]
+	print
+
+print "\n{0}\n  New Buildings \n{0}".format("-"*78)
+for nb in new['features']:
+	print nb['properties']
+
+print "\n\n{0}\n  Deleted Buildings \n{0}".format("-"*78)
+for ob in old['features']:
+	print ob['properties']
+
+
+for b in arcgis[:]:
+	
+'''
+	
+
+
+'''
 building_nums = []
 for b in buildings['features']:
 	# ignore bad numbers, names, geometry, and duplicates
@@ -127,3 +237,4 @@ for b in buildings:
 			print "{0}\n  Detected buildings not present in the authoritative source \n{0}".format("-"*78)
 			orphans_detected = True
 		print "{0} #{1}".format(b.name, b.number)
+'''
