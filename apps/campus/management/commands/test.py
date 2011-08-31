acf62b84f0f3f2c438a1eb4a48bfc4d0659ef764
Chris Conover <clcono@gmail.com>
2011-08-31 16:34:19 -0400

    Script to convert parking garage buildings to parking lot objects

--- apps/campus/management/commands/test.py
+++ apps/campus/management/commands/test.py
@@ -4,11 +4,42 @@
 '''
 from django.core.management.base import BaseCommand
 import os, campus, json
-from apps.campus.models import MapObj
+from apps.campus.models import Building, ParkingLot
 from django.core.management import call_command
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
 		
-		o = MapObj.objects.get(id=1)
-		print o.profile_link
+		STARTS_WITH_FILTER = 'Parking Garage'
+		
+		for building in Building.objects.filter(name__startswith = STARTS_WITH_FILTER):
+			print building.name
+			try:
+				parking_lot = ParkingLot.objects.get(name = building.name)
+			except ParkingLot.DoesNotExist:
+				print '>> No matching parking lot found'
+			else:
+				# Copy sketchup and abbreviation to parking lot and delete the building
+				building_dict = building.__dict__
+				building.delete()
+				
+				del building_dict['_state']
+				del building_dict['_mapobj_ptr_cache']
+				
+				building_dict['permit_type'] = parking_lot.permit_type
+				building_dict['number'] = parking_lot.number
+				building_dict['content_type_id'] = 4
+				
+				# Reverse the poly coordinates
+				#new_poly_coords = []
+				#for coord in building_dict['poly_coords']:
+				#	new_poly_coords.append(coord[1], coord[0])
+				#building_dict['poly_coords'] = new_poly_coords
+				
+				parking_lot.delete()
+				
+				new_lot = ParkingLot(**building_dict)
+				
+				
+				
+				new_lot.save()
