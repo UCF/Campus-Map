32fc302f7f9f08f2f33a1e6ce5434175d25f2a25
Chris Conover <clcono@gmail.com>
2012-07-24 10:12:40 -0400

    Create groubable location whenever a campus location is created

--- apps/campus/models.py
+++ apps/campus/models.py
@@ -292,8 +292,16 @@ class MapObj(models.Model):
 		
 		if(not self.content_type):
 			self.content_type = ContentType.objects.get_for_model(self.__class__)
+
 		super(MapObj, self).save(*args, **kwargs)
-	
+
+		if self.content_type.app_label == 'campus':
+			gl = GroupedLocation.objects.filter(content_type__pk=self.content_type.pk, object_pk=self.pk)
+			if not gl:
+				gl = GroupedLocation(content_type=self.content_type, object_pk=self.pk)
+				gl.save()
+
+			
 	def as_leaf_class(self):
 		content_type = self.content_type
 		model = content_type.model_class()
