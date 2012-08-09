cba2060df287fd28828ee5fa9b71c07b87137a22
Chris Conover <clcono@gmail.com>
2012-08-09 15:40:28 -0400

    Updated version number

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@
 import os
 import sys
 
-MAP_VERSION       = "1.6.7"
+MAP_VERSION       = "1.6.9"
 
 PROJECT_FOLDER    = os.path.dirname(os.path.abspath(__file__))
 APP_FOLDER        = os.path.join(PROJECT_FOLDER, 'apps')
