4c8698f87934e08a2bb4f89394a9edff687060d7
Brandon Groves <brandon.groves@gmail.com>
2014-06-10 15:28:28 -0400

    fixed urls typo

--- urls.py
+++ urls.py
@@ -40,7 +40,7 @@ urlpatterns += patterns('',
     # admin
     url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
     url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', 'django.contrib.auth.views.password_reset_confirm', name='password_reset_confi
+    url(r'^reset/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', 'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
     url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
     (r'^admin/', include(admin.site.urls)),
 
