6f086ccb8efd9af5f79bd8a3787a2f500f24582c
Brandon Groves <brandon.groves@gmail.com>
2014-06-11 12:59:38 -0400

    graceful fail if bus api isn't available

--- apps/campus/utils.py
+++ apps/campus/utils.py
@@ -166,7 +166,7 @@ class BusRouteAPI(object):
     app_code = ''
     cost_center_id = ''
     client = None
-    route_list = None
+    route_list = {}
 
     def __init__(self, wsdl_url, app_code, cost_center_id):
         self.app_code = app_code
@@ -177,13 +177,16 @@ class BusRouteAPI(object):
                 foo = context.envelope.getChild('Body').getChild[0]
                 foo.xmlns = "http://tempuri.org/"
 
-        self.client = Client(url=wsdl_url, plugins=[NamespacePlugin()])
+        try:
+            self.client = Client(url=wsdl_url, plugins=[NamespacePlugin()])
+        except urllib2.URLError:
+            pass #allow application to run without the feature
 
     def get_routes(self):
         """
         Get the list of routes.
         """
-        if self.route_list is None:
+        if not self.route_list and self.client is not None:
             print 'Retrieving Routes'
 
             routes_response = self.client.service.GetRoutes(sAppCode=self.app_code, sCostcenterId=self.cost_center_id, nDate=date.today().strftime('%Y%m%d'))
@@ -229,7 +232,7 @@ class BusRouteAPI(object):
         Get the route for a given route.
         """
         route_list = self.get_routes()
-        route_info = None
+        route_info = {}
         if route_id in route_list.keys():
             route_info = route_list[route_id]
 
@@ -239,13 +242,14 @@ class BusRouteAPI(object):
         """
         Get the routes stops.
         """
-        stops_response = self.client.service.GetStops(sAppCode=self.app_code, sCostCenterId=self.cost_center_id, nDate=date.today().strftime('%Y%m%d'), nShadowRouteId=route_id)
-        # Can't properly parse ampersand
-        stops_response = stops_response.replace('&', '&amp;');
-        xml_stop_list = ET.fromstring(stops_response)
         stops = []
-        for xml_stop in xml_stop_list:
-            stops.append(RouteStop(xml_stop.find('ShadowStopId').text, xml_stop.find('StopName').text, Point(lat=xml_stop.find('Lat').text, lon=xml_stop.find('Lon').text)))
+        if self.client is not None:
+            stops_response = self.client.service.GetStops(sAppCode=self.app_code, sCostCenterId=self.cost_center_id, nDate=date.today().strftime('%Y%m%d'), nShadowRouteId=route_id)
+            # Can't properly parse ampersand
+            stops_response = stops_response.replace('&', '&amp;');
+            xml_stop_list = ET.fromstring(stops_response)
+            for xml_stop in xml_stop_list:
+                stops.append(RouteStop(xml_stop.find('ShadowStopId').text, xml_stop.find('StopName').text, Point(lat=xml_stop.find('Lat').text, lon=xml_stop.find('Lon').text)))
         return stops
 
     def get_all_bus_stops_dict(self):
@@ -274,15 +278,16 @@ class BusRouteAPI(object):
         """
         Get the route polygon
         """
-        polygon_response = self.client.service.GetRoutePolygon(sAppCode=self.app_code, sCostcenterId=self.cost_center_id, nShadowRouteId=route_id)
         lines = []
-        if polygon_response is not None:
-            xml_polygon_list = ET.fromstring(polygon_response)
-            for xml_polygon in xml_polygon_list:
-                from_point = Point(lat=xml_polygon.find('FromLat').text, lon=xml_polygon.find('FromLon').text)
-                to_point = Point(lat=xml_polygon.find('ToLat').text, lon=xml_polygon.find('ToLon').text)
-                line = Line(from_point, to_point)
-                lines.append(line)
+        if self.client is not None:
+            polygon_response = self.client.service.GetRoutePolygon(sAppCode=self.app_code, sCostcenterId=self.cost_center_id, nShadowRouteId=route_id)
+            if polygon_response is not None:
+                xml_polygon_list = ET.fromstring(polygon_response)
+                for xml_polygon in xml_polygon_list:
+                    from_point = Point(lat=xml_polygon.find('FromLat').text, lon=xml_polygon.find('FromLon').text)
+                    to_point = Point(lat=xml_polygon.find('ToLat').text, lon=xml_polygon.find('ToLon').text)
+                    line = Line(from_point, to_point)
+                    lines.append(line)
         return lines
 
     def get_route_gps(self, route_id, stop_id=None):
@@ -293,19 +298,19 @@ class BusRouteAPI(object):
             stops = self.get_route_stops(route_id)
             if stops:
                 stop_id = stops[0].id
-            else:
-                raise Http404()
 
-        prediction_response = self.client.service.GetPredictions(sAppCode=self.app_code, sCostcenterId=self.cost_center_id, nShadowRouteId=route_id, nShadowStopId=stop_id, nMaxPredictions=10)
-        xml_prediction_list = ET.fromstring(prediction_response)
         gps_list = []
-        vehicles = []
-        for xml_prediction in xml_prediction_list:
-            if xml_prediction.find('GPSTime').text and xml_prediction.find('Vehicle').text and xml_prediction.find('Vehicle').text not in vehicles:
-                vehicles.append(xml_prediction.find('Vehicle').text);
-                gps_list.append(BusGps(xml_prediction.find('Vehicle').text,
-                                       Point(xml_prediction.find('Lat').text, xml_prediction.find('Lon').text),
-                                       xml_prediction.find('NextStop').text))
+        if self.client is not None and stop_id is not None:
+            prediction_response = self.client.service.GetPredictions(sAppCode=self.app_code, sCostcenterId=self.cost_center_id, nShadowRouteId=route_id, nShadowStopId=stop_id, nMaxPredictions=10)
+            xml_prediction_list = ET.fromstring(prediction_response)
+            vehicles = []
+            for xml_prediction in xml_prediction_list:
+                if xml_prediction.find('GPSTime').text and xml_prediction.find('Vehicle').text and xml_prediction.find('Vehicle').text not in vehicles:
+                    vehicles.append(xml_prediction.find('Vehicle').text);
+                    gps_list.append(BusGps(xml_prediction.find('Vehicle').text,
+                                           Point(xml_prediction.find('Lat').text, xml_prediction.find('Lon').text),
+                                           xml_prediction.find('NextStop').text))
+
 
         return gps_list
 
--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@
 import os
 import sys
 
-MAP_VERSION       = "1.7.1"
+MAP_VERSION       = "1.7.2"
 
 PROJECT_FOLDER    = os.path.dirname(os.path.abspath(__file__))
 APP_FOLDER        = os.path.join(PROJECT_FOLDER, 'apps')
