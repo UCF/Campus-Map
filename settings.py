3b4def957ee81c37d30ed4fd7e0f3c834849cc81
Brandon Groves <brandon.groves@gmail.com>
2014-11-12 10:05:45 -0500

    updated version

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@
 import os
 import sys
 
-MAP_VERSION       = "1.7.24"
+MAP_VERSION       = "1.7.25"
 
 PROJECT_FOLDER    = os.path.dirname(os.path.abspath(__file__))
 APP_FOLDER        = os.path.join(PROJECT_FOLDER, 'apps')
