ea4abc978f0c8900efa7af3800b12f1463a0d367
RJ Bruneel <rj.bruneel@ucf.edu>
2019-05-22 16:54:06 -0400

    removed unused imports

--- apps/campus/management/commands/import_address_data.py
+++ apps/campus/management/commands/import_address_data.py
@@ -1,10 +1,6 @@
 import csv
 
-from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
-from django.db import connection, transaction
-from django.db.utils import IntegrityError, DatabaseError
-
+from django.core.management.base import BaseCommand
 from apps.campus.models import Building
 
 class Command(BaseCommand):
